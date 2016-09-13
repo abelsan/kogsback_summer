13531156470023a3cc6f63ee476a56812db3edcb
abelsan <abel@mit.edu>
2016-09-12 22:33:41 -0400

    added mongo & forever

--- src/config/connections.js
+++ src/config/connections.js
@@ -40,13 +40,14 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  mongodbServer: {
+    adapter: 'sails-mongo',
+    host: 'mongo',
+    port: 27017,
+    // user: 'username', //optional
+    // password: 'password', //optional
+    database: 'test' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
